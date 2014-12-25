5cd5b15ea8879577559dec8be0562ab7f2c4577e
Jaroslaw Zabiello <hipertracker@gmail.com>
2014-12-25 17:34:28 +0000

    Enabled support for experimental ES7 features

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = {
     // Ignore all files within node_modules
     // 6to5 files can be .js and .es6
     if (filename.indexOf("node_modules") === -1 && to5.canCompile(filename)) {
-      return to5.transform(src, { filename: filename }).code;
+      return to5.transform(src, { filename: filename, experimental: true }).code;
     }
     return src;
   }
