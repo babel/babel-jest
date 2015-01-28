522fa2b506d710fb12007bd95366e2212baf1171
Sebastian McKenzie <sebmck@gmail.com>
2015-01-28 13:42:14 +1100

    upgrade to 6to5 3.0.0

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var to5 = require("6to5-core");
 module.exports = {
   process: function (src, filename) {
     // Ignore all files within node_modules
-    // 6to5 files can be .js and .es6
+    // 6to5 files can be .js, .es, .jsx or .es6
     if (filename.indexOf("node_modules") === -1 && to5.canCompile(filename)) {
       return to5.transform(src, { filename: filename }).code;
     }
