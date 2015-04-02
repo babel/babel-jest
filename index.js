5a50ac780d21eb30e86b68e5c7732e9725ad93e0
Randy Lebeau <randylebeau@gmail.com>
2015-04-01 23:01:54 -0400

    Fixed segmentation fault error

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var babel = require("babel-core");
 
 module.exports = {
-  process: function (src, filename) {
-    // Ignore all files within node_modules
-    // babel files can be .js, .es, .jsx or .es6
-    if (filename.indexOf("node_modules") === -1 && babel.canCompile(filename)) {
-      return babel.transform(src, { filename: filename }).code;
-    }
-    return src;
-  }
+	process: function(src, filename) {
+		// Ignore all files within node_modules
+		// babel files can be .js, .es, .jsx or .es6
+		if (filename.indexOf("node_modules") === -1 && babel.canCompile(filename)) {
+			// return babel.transform(src, { filename: filename }).code;
+			babel.transformFile("filename.js", function(err, result) {
+				if(err) {
+					return new Error(err);
+				}
+				return result.code;
+			});
+		} else {
+			return src;
+		}
+	}
 };
