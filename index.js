e73d780992c36d0c214ae716de20b09aa9e418f3
Randy Lebeau <randylebeau@gmail.com>
2015-04-01 23:15:38 -0400

    remove test string

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = {
 		// babel files can be .js, .es, .jsx or .es6
 		if (filename.indexOf("node_modules") === -1 && babel.canCompile(filename)) {
 			// return babel.transform(src, { filename: filename }).code;
-			babel.transformFile("filename.js", function(err, result) {
+			babel.transformFile(filename, function(err, result) {
 				if(err) return new Error(err);
 				return result.code;
 			});
