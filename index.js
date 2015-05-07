6bc777f86abd2675dc2ea775617b31ea9c493759
Richard Goater <richard.goater27@gmail.com>
2015-05-07 22:34:31 +0100

    Add environment variable for Babel runtime

--- index.js
+++ index.js
@@ -7,10 +7,20 @@ module.exports = {
     // no environment variable is specified.
     var stage = process.env.BABEL_JEST_STAGE || 2;
 
+    // Allow the Babel runtime to be included via an
+    // environment variable. Remember to add
+    // node_modules/babel to unmockedModulePathPatterns
+    var optional = process.env.BABEL_JEST_RUNTIME ? [ "runtime" ] : undefined;
+
     // Ignore all files within node_modules
     // babel files can be .js, .es, .jsx or .es6
     if (filename.indexOf("node_modules") === -1 && babel.canCompile(filename)) {
-      return babel.transform(src, { filename: filename, stage: stage, retainLines: true }).code;
+      return babel.transform(src, {
+        filename: filename,
+        stage: stage,
+        retainLines: true,
+        optional: optional
+      }).code;
     }
 
     return src;
