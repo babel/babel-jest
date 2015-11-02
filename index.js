dc3bcb751a42420949f56f0190f0f9a31b4ea8c5
alvinsj <email.to.alvin@gmail.com>
2015-11-02 21:44:33 +0700

    fix api change, remove invalid options

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
-var babel = require("babel-core");
+var babel = require("babel-core"),
+    canCompile = babel.util.canCompile;
 
 module.exports = {
   process: function (src, filename) {
-    // Allow the stage to be configured by an environment
-    // variable, but use Babel's default stage (2) if
-    // no environment variable is specified.
-    var stage = process.env.BABEL_JEST_STAGE || 2;
-
     // Ignore all files within node_modules
     // babel files can be .js, .es, .jsx or .es6
-    if (filename.indexOf("node_modules") === -1 && babel.canCompile(filename)) {
+    if (filename.indexOf("node_modules") === -1 && canCompile(filename)) {
       return babel.transform(src, {
         filename: filename,
-        stage: stage,
-        retainLines: true,
-        auxiliaryCommentBefore: "istanbul ignore next"
+        retainLines: true
       }).code;
     }
 
